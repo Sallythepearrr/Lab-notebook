5bbba208293a568a5786a5fbae30776a2148e4bc
Sally Zhang <sallyzhang010804@gmail.com>
2025-11-20 12:01:36 +0100

    change path

---
+++ Stimulus-Evoked-Activity_test.py
@@ -0,0 +1,75 @@
+# %% [markdown]
+# # Trial Averaging
+
+# %%
+import os, sys
+sys.path += ['physion/src'] # add src code directory for physion
+import physion
+import physion.utils.plot_tools as pt
+pt.set_style('dark')
+
+
+# %% [markdown]
+# ## Load data
+
+# %%
+# load a datafile
+filename = os.path.join(os.path.expanduser('~'), 'DATA', 
+                        'physion_Demo-Datasets', 'SST-WT', 'NWBs',
+                        '2023_02_15-13-30-47.nwb')
+
+data = physion.analysis.read_NWB.Data(filename,
+                                     verbose=False)
+
+# %% [markdown]
+# ## Build episodes (stimulus-aligned)
+
+# %%
+
+# find protocol of full-field gratings
+p_name = [p for p in data.protocols if 'ff-gratings' in p][0]
+episodes = physion.analysis.process_NWB.EpisodeData(data, 
+                                                    quantities=['dFoF',
+                                                                'pupil_diameter'],
+                                                    protocol_name=p_name)
+
+# %% [markdown]
+# ## Plot properties
+
+# %%
+# plot over varying angles
+plot_props = dict(column_key='angle',
+                  with_annotation=True,
+                  figsize=(9,1.8))
+
+# %% [markdown]
+# ## Pupil variations
+
+# %%
+fig, AX = physion.dataviz.episodes.trial_average.plot(episodes,
+                                                      quantity='pupil_diameter',
+                                                    #   with_std=False,
+                                                      **plot_props)
+# %% [markdown]
+# ## Average over all ROIs 
+
+# %%
+fig, AX = physion.dataviz.episodes.trial_average.plot(episodes,
+                                                      quantity='dFoF',
+                                                      roiIndex=range(data.nROIs),
+                                                      **plot_props)
+
+# %% [markdown]
+# ## Single ROIs 
+
+# %%
+fig, AX = physion.dataviz.episodes.trial_average.plot(episodes,
+                                                      roiIndex=0,
+                                                      **plot_props)
+
+# %%
+fig, AX = physion.dataviz.episodes.trial_average.plot(episodes,
+                                                      roiIndex=2,
+                                                      **plot_props)
+
+# %%
