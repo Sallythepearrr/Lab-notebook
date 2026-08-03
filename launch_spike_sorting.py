9772b15accdd1c8f24630a358e42205cab89aa5e
yann <yann.zerlautr@proton.me>
2026-08-03 17:43:38 +0200

    adding spike through spike interface

---
+++ launch_spike_sorting.py
@@ -0,0 +1,21 @@
+# %%
+import os, sys
+import spikeinterface.sorters as ss
+import spikeinterface.full as si
+
+for folder in os.listdir(sys.argv[-1]):
+
+    print()
+    print('    running "%s" ' % folder)
+    print()
+
+    rec = si.read_openephys(\
+        os.path.join(sys.argv[-1], folder),
+        stream_name='Record Node 101#OneBox-100.ProbeA')
+
+    sorting = ss.run_sorter(sorter_name='kilosort4', 
+                            recording=rec,
+                            verbose=True,
+                            folder=os.path.join(sys.argv[-1], 
+                                                'kilosort4_output'))
+
