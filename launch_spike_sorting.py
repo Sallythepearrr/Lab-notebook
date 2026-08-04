17fa995a92713574f0537fa0bcfe121381fa5aa4
yann <yann.zerlautr@proton.me>
2026-08-04 14:14:27 +0200

    adding-rm-folder-check

--- launch_spike_sorting.py
+++ launch_spike_sorting.py
@@ -1,23 +1,33 @@
 # %%
-import os, sys
+import os, sys, shutil
 import spikeinterface.sorters as ss
 import spikeinterface.full as si
 
-for folder in os.listdir(sys.argv[-1]):
+folder  = sys.argv[-1]
+rec_name = folder.split(os.path.sep)[-1]
 
-    print()
-    print('    running "%s" ' % folder)
-    print()
+print()
+print('    running "%s" ' % folder)
+print('                   ', rec_name)
+print()
 
-    rec_name = folder.split(os.path.sep)[-1]
 
-    rec = si.read_openephys(\
-        os.path.join(sys.argv[-1], folder),
-        stream_name='Record Node 101#OneBox-100.ProbeA')
+rec = si.read_openephys(\
+    os.path.join(sys.argv[-1], folder),
+    stream_name='Record Node 101#OneBox-100.ProbeA')
 
-    sorting = ss.run_sorter(sorter_name='kilosort4', 
-                            recording=rec,
-                            verbose=True,
-                            folder=os.path.join(sys.argv[-1], 
-                                                'kilosort4_%s' % rec_name))
+
+rec = rec.select_channels(rec.get_channel_ids()[:250])
+
+
+ks_folder=os.path.join(sys.argv[-1], 'kilosort4_%s' % rec_name)
+if os.path.isdir(ks_folder):
+    y = input(' folder "%s" already exists ! \n Do you want to delete it ? y/[n]' % ks_folder)
+    if y in ['y', 'yes']:
+        shutil.rmtree(ks_folder)
+
+sorting = ss.run_sorter(sorter_name='kilosort4', 
+                        recording=rec,
+                        verbose=True,
+                        folder=ks_folder)
 
